60a7d7ccd55e986d2b26ee0a00bef45fc359d95c
Nabihah Sema <st10440590@vcconnect.edu.za>
2025-04-04 16:05:27 +0200

    Add project files.

---
+++ EventEase/Models/Venue.cs
@@ -0,0 +1,49 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EventEase.Models
+{
+    public class Venue
+    {
+        [Key]
+
+
+        [Display(Name = "Venue ID")]
+        [DataType(DataType.Text)]
+        public int VenueID { get; set; }
+
+
+
+        [Display(Name = "Name of Venue")]
+        [DataType(DataType.Text)]
+        [StringLength(60, MinimumLength = 3)]
+        [Required]
+        public string? VenueName { get; set; }
+
+
+
+        [Display(Name = "Location of Venue")]
+        [DataType(DataType.MultilineText)]
+        [StringLength(100, MinimumLength = 3)]
+        [Required]
+        public string? VenueLocation { get; set; }
+
+
+
+        [Display(Name = "Maximum Capacity")]
+        [DataType(DataType.Text)]
+        [Range(1, 1000)]
+        [Required]
+        public int VenueCapacity { get; set; }
+
+
+        [Display(Name = "Image")]
+        public string? VenueImage { get; set; }
+
+
+
+        //navigation properties 
+        public List<Booking>? Bookings { get; set; }
+
+
+    }
+}
